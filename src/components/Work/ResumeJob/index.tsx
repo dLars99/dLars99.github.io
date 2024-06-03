b071011cd24576215284de63a9c49f0075b02fcb
David Larsen <54251395+dLars99@users.noreply.github.com>
2024-06-03 07:25:27 -0500

    feat(work): add work section (#10)
    * feat(work): outline work page
    
    * feat(work): add open close icons
    
    * feat(work): add styles to resume blocks
    
    * feat(work): add job data for work items
    
    * fix(work): add missing keys to job mappings
    
    * fix(job): reorganize and fix margins
    
    * feat(work): update layout for earlier experiences
    
    * feat(work): add additional bullet points to resume items
    
    * fix(main): adjust bottom spacing
    
    * feat(work): add downloadable resume
    
    * fix(work): fix job width on mobile

---
+++ src/components/Work/ResumeJob/index.tsx
@@ -0,0 +1,51 @@
+import React, { FC, ReactNode } from "react";
+import {
+  Box,
+  Button,
+  Collapse,
+  Flex,
+  Heading,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { FaExpand } from "@react-icons/all-files/fa/FaExpand";
+import { FaWindowMinimize } from "@react-icons/all-files/fa/FaWindowMinimize";
+import { Job } from "../jobData";
+import JobContent from "../JobContent";
+
+export interface ResumeJobProps {
+  children?: ReactNode;
+  job: Job;
+}
+
+const ResumeJob: FC<ResumeJobProps> = ({ children, job }) => {
+  const { isOpen, onToggle } = useDisclosure();
+
+  return (
+    <Box my={8}>
+      <Flex
+        alignItems="center"
+        bg="background.transparent"
+        px={6}
+        py={4}
+        justifyContent="space-between"
+        width="100%"
+      >
+        <Heading as="h3" fontSize="2xl">
+          {job.name}
+        </Heading>
+
+        <Button bg="transparent" onClick={onToggle}>
+          {isOpen ? <FaWindowMinimize /> : <FaExpand />}
+        </Button>
+      </Flex>
+
+      <Collapse in={isOpen}>
+        {children}
+
+        <JobContent job={job} />
+      </Collapse>
+    </Box>
+  );
+};
+
+export default ResumeJob;
--- src/pages/work.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const WorkPage = () => (
-  <main>
-    <h1>Resume</h1>
-  </main>
-);
-
-export default WorkPage;
