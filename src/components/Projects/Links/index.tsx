33c59b6f2060ddf2b16d3b9da9092075927ebba9
David Larsen <davidplarsen@gmail.com>
2024-06-07 07:21:29 -0500

    feat(projects): abstract links component

---
+++ src/components/Projects/Links/index.tsx
@@ -0,0 +1,48 @@
+import React, { FC } from "react";
+import { Flex, Link } from "@chakra-ui/react";
+import { StaticImage } from "gatsby-plugin-image";
+
+type ProjectLink = {
+  name: string;
+  url: string;
+  type: LinkType;
+};
+
+type LinkType = "github" | "npm";
+
+interface RrtLinksProps {
+  links: ProjectLink[];
+}
+
+const linkDisplayNames = {
+  github: "GitHub",
+  npm: "NPM",
+};
+
+const linkImages = {
+  github: "../../../images/github.png",
+  npm: "../../../images/npm.png",
+};
+
+const RrtLinks: FC<RrtLinksProps> = ({ links }) => {
+  const getAltText = (name: string, type: LinkType) =>
+    `Link to ${name} on ${linkDisplayNames[type]}`;
+
+  return links.length > 0 ? (
+    <Flex justifyContent="center" gap={12} my={4}>
+      {links.map((link) => (
+        <Link isExternal href={link.url} key={link.name}>
+          <StaticImage
+            alt={getAltText(link.name, link.type)}
+            src={linkImages[link.type]}
+            placeholder="blurred"
+            width={40}
+            height={40}
+          />
+        </Link>
+      ))}
+    </Flex>
+  ) : null;
+};
+
+export default RrtLinks;
