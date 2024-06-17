6ad03b2d3cec73c6ba8a0b544cc876af97cec25a
David Larsen <davidplarsen@gmail.com>
2024-06-17 07:40:26 -0500

    feat(projects): update barkeep description

--- src/components/Projects/Barkeep/BarkeepDescription.tsx
+++ src/components/Projects/Barkeep/BarkeepDescription.tsx
@@ -1,26 +1,44 @@
 import React from "react";
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Link, ListItem, Text, UnorderedList } from "@chakra-ui/react";
 
 const BarkeepDescription = () => (
   <Box pr={[12, 16]}>
-    <Text>Initial completion: May 2024</Text>
+    <Text>Initial completion: July 2022</Text>
+    <Text>Last update: October 2022</Text>
 
-    <Text>An open-source tab library of my creation</Text>
+    <Text>A cocktail catalog for personal use</Text>
 
     <Box>
       <Text>
-        Creating tabs for your router is easy. Making them meet WAI-ARIA
-        accessibility standards is not.
+        Several years ago, my wife got into mixology, especially tiki cocktails.
+        A typical tiki drink includes many ingredients. This presents two
+        challenges:
+      </Text>
+
+      <UnorderedList>
+        <ListItem>
+          Keeping track of drink recipes from many different sources
+        </ListItem>
+        <ListItem>
+          Finding a recipe that matches the ingredients you have
+        </ListItem>
+      </UnorderedList>
+
+      <Text>
+        Barkeep is our in-home solution to these problems. At its core is a
+        database of drink recipes we have collected. They can be searched by
+        regular searches, but also filtered by one or more ingredients to find
+        the right recipe for the moment.
       </Text>
 
       <Text>
-        Lots of tab libraries give you accessible tabs. Connecting them to your
-        router, though, means jumping through lots of hoops.
-      </Text>
-
-      <Text>
-        React Routing Tabs gives you accessible tabs that connect to your router
-        with minimal effort.
+        Since this is for my personal use, and due to legal implications around
+        alcohol, it is not publicly deployed. I run this in a Docker container
+        on a home
+        <Link isExternal href="https://unraid.net" mx="0.25rem">
+          UnRaid
+        </Link>
+        server.
       </Text>
     </Box>
   </Box>
