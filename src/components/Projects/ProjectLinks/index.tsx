347871f8757de52caf529330cc72efd80cd08c19
David Larsen <davidplarsen@gmail.com>
2024-06-21 06:52:01 -0500

    feat(projects): use dynamicimage for project links

--- src/components/Projects/ProjectLinks/index.tsx
+++ src/components/Projects/ProjectLinks/index.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from "react";
 import { Flex, Link } from "@chakra-ui/react";
-import { StaticImage } from "gatsby-plugin-image";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import { graphql, useStaticQuery } from "gatsby";
 
 export type ProjectLink = {
   name: string;
@@ -19,28 +20,43 @@ const linkDisplayNames = {
   npm: "NPM",
 };
 
-const linkImages = {
-  github: "../../../images/github.png",
-  npm: "../../../images/npm.png",
-};
-
 const ProjectLinks: FC<RrtLinksProps> = ({ links }) => {
+  const imageData = useStaticQuery(graphql`
+    query {
+      allFile(filter: { sourceInstanceName: { eq: "images" } }) {
+        edges {
+          node {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const parseImage = (type: LinkType) => {
+    const edgeWithImageNode = imageData.allFile.edges.find((edge: any) =>
+      edge.node.childImageSharp.gatsbyImageData.images.fallback.src.includes(
+        type
+      )
+    );
+    return getImage(edgeWithImageNode.node);
+  };
+
   const getAltText = (name: string, type: LinkType) =>
     `Link to ${name} on ${linkDisplayNames[type]}`;
 
   return links.length > 0 ? (
     <Flex justifyContent="center" gap={12} my={4}>
-      {links.map((link) => (
-        <Link isExternal href={link.url} key={link.name}>
-          <StaticImage
-            alt={getAltText(link.name, link.type)}
-            src={linkImages[link.type]}
-            placeholder="blurred"
-            width={40}
-            height={40}
-          />
-        </Link>
-      ))}
+      {links.map((link) => {
+        const image = parseImage(link.type);
+        return image ? (
+          <Link isExternal href={link.url} key={link.name}>
+            <GatsbyImage alt={getAltText(link.name, link.type)} image={image} />
+          </Link>
+        ) : null;
+      })}
     </Flex>
   ) : null;
 };
